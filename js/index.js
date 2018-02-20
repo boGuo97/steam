0516689d4039c97f9fc04ed05676e99f1601a1b5
1781324621@qq.com <1781324621@qq.com>
2018-02-20 23:02:46 +0800

    提交

--- js/index.js
+++ js/index.js
@@ -19,36 +19,54 @@ $(".img-1").mouseenter(function(){
 	}
 })
 // .........................................................................
+$Carone = $(".carousel-one")
+$Cartow = $(".carousel-two")
+$Carsan = $(".carousel-san")
 //  第一个carousel-center
 $boxOne = $(".carousel-one .carousel-center")
-// 第二个carousel-center
+//  第二个carousel-center
 $boxTow = $(".carousel-two .carousel-center")
-// 第一个右键
+//  第三个carousel-center
+$boxSan = $(".carousel-san .carousel-center")
+//  第一个右键
 $RightOne = $(".carousel-one .arr-rigth")
-// 第二个右键
+//  第二个右键
 $RightTow = $(".carousel-two .arr-rigth")
-// 第一个左键
+//  第三个右键
+$RightSan = $(".carousel-san .arr-rigth")	
+//  第一个左键
 $LeftOne = $(".carousel-one .arr-left")
-// 第二个左键
+//  第二个左键
 $LeftTow = $(".carousel-two .arr-left")
-//第一个小圆点
+//  第三个左键
+$LeftSan = $(".carousel-san .arr-left")
+//  第一个小圆点
 $DotOne = $(".carousel-one .dot")
-// 第二个小圆点
+//  第二个小圆点
 $DotTow = $(".carousel-two .dot")
+//  第三个小圆点
+$DotSan = $(".carousel-san .dot")
 // ..........................................................................
-// 第一个点击事件
+//  第一个点击事件
 $RightOne.click(function(){
-	Right($boxOne,$spanListOne)
+	Right($boxOne,$spanListOne,$Carone)
 })
 $LeftOne.click(function(){
-	Left($boxOne,$spanListOne)
+	Left($boxOne,$spanListOne,$Carone)
 })
 // 第二个点击事件 
 $RightTow.click(function(){
-	Right($boxTow,$spanListTow)
+	Right($boxTow,$spanListTow,$Cartow)
 })
 $LeftTow.click(function(){	
-	Left($boxTow,$spanListTow)
+	Left($boxTow,$spanListTow,$Cartow)
+})
+// 	第三个点击事件
+$RightSan.click(function(){
+	Right($boxSan,$spanListSan,$Carsan)
+})
+$LeftSan.click(function(){	
+	Left($boxSan,$spanListSan,$Carsan)
 })
 // ..........................................................................
 $(".img-1").mouseleave(function(){
@@ -59,38 +77,94 @@ $(".img-1").mouseleave(function(){
 		background : ""
 	})
 })
-var b = 0;
-function Right(obj,EnenList){
+var b = 0;// 第一个轮播图指标
+var l = 0;// 第二个轮播图指标
+var e = 0;// 第三个轮播图指标
+function Right(obj,EnenList,age3){
 	for(var n = 0;n < obj.length;n++){
 		obj.eq(n).hide()
 		EnenList.removeClass("dot-span")
 	}
-	if(b < obj.length-1){
-		b++;
-		obj.eq(b).fadeIn(500)
-		EnenList.eq(b).addClass("dot-span")
+	if(age3 == $Carone){
+		if(b < obj.length-1){
+			b++;
+			obj.eq(b).fadeIn(500)
+			EnenList.eq(b).addClass("dot-span")
+		}
+		else{
+			b = 0;
+			obj.eq(b).fadeIn(500)	
+			EnenList.eq(b).addClass("dot-span")
+		}
 	}
-	else{
-		b = 0;
-		obj.eq(b).fadeIn(500)	
-		EnenList.eq(b).addClass("dot-span")
+	if(age3 == $Cartow){
+		if(l < obj.length-1){
+			l++;
+			obj.eq(l).fadeIn(500)
+			EnenList.eq(l).addClass("dot-span")
+		}
+		else{
+			l = 0;
+			obj.eq(l).fadeIn(500)	
+			EnenList.eq(l).addClass("dot-span")
+		}
+	}
+	if(age3 == $Carsan){
+		if(e < obj.length-1){
+			e++;
+			obj.eq(e).fadeIn(500)
+			EnenList.eq(e).addClass("dot-span")
+		}
+		else{
+			e = 0;
+			obj.eq(e).fadeIn(500)	
+			EnenList.eq(e).addClass("dot-span")
+		}
 	}
 }
-function Left(obj,EnenList){
+function Left(obj,EnenList,age3){
 	for(var n = 0;n < obj.length;n++){
 		obj.eq(n).hide()
 		EnenList.removeClass("dot-span")
 	}
-	if(b !== 0){
-		b--
-		// obj.eq(b).hide()
-		obj.eq(b).fadeIn(500)				
-		EnenList.eq(b).addClass("dot-span")
+	if(age3 == $Carone){
+		if(b !== 0){
+			b--
+			// obj.eq(b).hide()
+			obj.eq(b).fadeIn(500)				
+			EnenList.eq(b).addClass("dot-span")
+		}
+		else{
+			b = obj.length-1;
+			obj.eq(b).fadeIn(500)	
+			EnenList.eq(b).addClass("dot-span")
+		}
 	}
-	else{
-		b = obj.length-1;
-		obj.eq(b).fadeIn(500)	
-		EnenList.eq(b).addClass("dot-span")
+	if(age3 == $Cartow){
+		if(l !== 0){
+			l--
+			// obj.eq(b).hide()
+			obj.eq(l).fadeIn(500)				
+			EnenList.eq(l).addClass("dot-span")
+		}
+		else{
+			l = obj.length-1;
+			obj.eq(l).fadeIn(500)	
+			EnenList.eq(l).addClass("dot-span")
+		}
+	}
+	if(age3 == $Carsan){
+		if(e !== 0){
+			e--
+			// obj.eq(b).hide()
+			obj.eq(e).fadeIn(500)				
+			EnenList.eq(e).addClass("dot-span")
+		}
+		else{
+			e = obj.length-1;
+			obj.eq(e).fadeIn(500)	
+			EnenList.eq(e).addClass("dot-span")
+		}
 	}
 }
   // 移上  下 显示隐藏  弹出层
@@ -102,12 +176,19 @@ $boxOne.mouseleave(function(){
 	$(".side").hide()
 	clearInterval(UI)
 	UY = setInterval(function(){
-		Right($boxOne,$spanListOne)
+		Right($boxOne,$spanListOne,$Carone)
 	},4000)
+})
+$boxTow.mouseenter(function(){
+	clearInterval(UP)
+})
+$boxTow.mouseleave(function(){
+	UP = setInterval(function(){
+		Right($boxTow,$spanListTow,$Cartow)
+	},3000)
 })
 // ...........................................小图轮播
 var k = 0;
-var l = 0;
 // ............................................................................
 //  生成小圆点
 for(var n = 0;n < $boxOne.length;n++){
@@ -120,14 +201,23 @@ for(var n = 0;n < $boxTow.length;n++){
 	$dot.attr("index",n)
 	$dot.appendTo($DotTow)
 }
+for(var n = 0;n < $boxSan.length;n++){
+	var $dot = $("<span></span>")
+	$dot.attr("index",n)
+	$dot.appendTo($DotSan)
+}
 //  第一个dot中的span
 $spanListOne = $DotOne.find("span")
 //  第二个dot中的span
 $spanListTow = $DotTow.find("span")
+//  第三个dot中的sapn
+$spanListSan = $DotSan.find("span")
 // 	给第一个dot中的span的第一个上色
 $spanListOne.eq(0).addClass("dot-span")
-// 	给第一个dot中的span的第一个上色
+// 	给第二个dot中的span的第一个上色
 $spanListTow.eq(0).addClass("dot-span")
+//  给第三个dot中的span的第一个上色
+$spanListSan.eq(0).addClass("dot-span")
 //  小圆点点击事件
 $spanListOne.click(function(){
 	Sum = Number($(this).attr("index"))
@@ -144,10 +234,20 @@ $spanListTow.click(function(){
 	for(var n = 0;n < $boxTow.length;n++){
 		$boxTow.eq(n).hide()
 	}
-	b = 0 
-	b = Sum;
-	$boxTow.eq(b).fadeIn(100)
+	l = 0 
+	l = Sum;
+	$boxTow.eq(l).fadeIn(100)
 	yidong($spanListTow)
+})
+$spanListSan.click(function(){
+	Sum = Number($(this).attr("index"))
+	for(var n = 0;n < $boxSan.length;n++){
+		$boxSan.eq(n).hide()
+	}
+	e = 0 
+	e = Sum;
+	$boxSan.eq(e).fadeIn(100)
+	yidong($spanListSan)
 })
 function yidong(DOT){
 	DOT.removeClass("dot-span")
@@ -155,8 +255,12 @@ function yidong(DOT){
 }
 // ........自动播放................................................................
 UY = setInterval(function(){
-		Right($boxOne,$spanListOne)
+	Right($boxOne,$spanListOne,$Carone)
 },2000)
+UP = setInterval(function(){
+	Right($boxTow,$spanListTow,$Cartow)
+},3000)
+
 // ..................智能判断..............\
 
 $boxOne.mouseenter(function(){
@@ -174,5 +278,19 @@ $boxOne.mouseenter(function(){
 		$sideList.eq(k).fadeIn()
 	},1000)
 })
-// ........................................轮播图2.....................		
+// ......................................................................
+$(".tab-a").mouseenter(function(){
+	for(var j = 0;j < $(".tab-a").length;j++){
+		$(".tab-a").eq(j).attr("index",j)
+		$(".tab-right").eq(j).fadeOut();
+	}
+		var Num = $(this).attr("index")
+		$(".tab-right").eq(Num).fadeIn()
+		$(".tab-a").removeClass("tab-a-one")
+		$(this).addClass("tab-a-one")
+		$(this)<$(".fable").addClass("fable-one")
+		$(this)<$(".win span").addClass("span-one")
+		$(this)<$(".win em").addClass("em-one")
+})
+
 
